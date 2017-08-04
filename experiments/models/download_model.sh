2a9e6bace7038c489a43206241c226945b8ef054
Zhang <hangzha@186590d9d5fb.ant.amazon.com>
2017-08-03 21:34:47 -0700

    url

--- experiments/models/download_model.sh
+++ experiments/models/download_model.sh
@@ -1,3 +1,3 @@
 cd models
-wget -O 21styles.model https://www.dropbox.com/s/6henvvnm6k5qows/21styles.model?dl=1
+wget -O 21styles.model https://www.dropbox.com/s/2iz8orqqubrfrpo/21styles.model?dl=1
 cd ..
