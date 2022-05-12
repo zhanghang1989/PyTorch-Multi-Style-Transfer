75d256049a6af7ceccae01bd5c472556478048ea
Hang Zhang <zhanghang0704@gmail.com>
2022-05-11 18:33:54 -0700

    Update download_model.sh

--- experiments/models/download_model.sh
+++ experiments/models/download_model.sh
@@ -1,3 +1,3 @@
 cd models
-wget -O 21styles.model https://www.dropbox.com/s/2iz8orqqubrfrpo/21styles.model?dl=1
+wget -O 21styles.model https://github.com/zhanghang1989/PyTorch-Multi-Style-Transfer/releases/download/v0.1/21styles.model
 cd ..
